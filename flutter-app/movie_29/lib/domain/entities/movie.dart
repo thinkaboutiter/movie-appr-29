cd0a7467a58d8788440e85b8e057f60ae36c3cca
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-04-29 15:42:58 +0300

    2: add abstract movie entity type contract

---
+++ flutter-app/movie_29/lib/domain/entities/movie.dart
@@ -0,0 +1,11 @@
+abstract class Movie {
+  String get id;
+  String get title;
+  String get year;
+  List<String> get genres;
+  String get posterUrl;
+  double get rating;
+  String get storyline;
+  List<String> get actors;
+  String get duration;
+}
