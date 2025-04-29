d8468b1db6e743b6a99f3a9ccfeda2f0b82cffef
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-04-29 20:01:37 +0300

    3: add main screen and Movie29App

---
+++ flutter-app/movie_29/lib/presentation/screens/main_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:movie_29/utils/constants.dart';
+import '../../data/datasources/movie_network_datasource.dart';
+import '../../data/datasources/movie_stub_datasource.dart';
+import '../../data/datasources/user_local_datasource.dart';
+import '../../data/repositories/movies_repository.dart';
+import '../../presentation/screens/movies_list_screen.dart';
+import '../../presentation/screens/user_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late final MovieStubDataSource _stubDataSource;
+  late final MovieNetworkDataSource _networkDataSource;
+  late final MoviesRepository _moviesRepository;
+  late final UserLocalDataSource _userDataSource;
+  
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _stubDataSource = MovieStubDataSource();
+    _networkDataSource = MovieNetworkDataSource(
+      apiUrl: ApiEndpoints.movies,
+    );
+    _moviesRepository = MoviesRepository(
+      networkDataSource: _networkDataSource,
+      stubDataSource: _stubDataSource,
+    );
+    _userDataSource = UserLocalDataSource();
+  }
+
+  @override
+  void dispose() {
+    _moviesRepository.dispose();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          MoviesListScreen(repository: _moviesRepository),
+          UserScreen(
+            userDataSource: _userDataSource,
+            moviesRepository: _moviesRepository,
+          ),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onItemTapped,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.movie_outlined),
+            selectedIcon: Icon(Icons.movie),
+            label: 'Movies',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
