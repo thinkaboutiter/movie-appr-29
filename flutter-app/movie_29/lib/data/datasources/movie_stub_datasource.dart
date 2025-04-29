99044bb346cb9764ded8c7b33dea80b4b294f4f2
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-04-29 16:26:58 +0300

    2: add movies data sources and reposotory

---
+++ flutter-app/movie_29/lib/data/datasources/movie_stub_datasource.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import '../../domain/entities/movie_app_e.dart';
+import '../../domain/entities/movie_network_e.dart';
+
+
+class MovieStubDataSource {
+  Future<List<MovieAppE>> getMovies() async {
+    try {
+      // Load the bundled JSON file
+      final jsonString = await rootBundle.loadString('assets/movies.json');
+      
+      // Parse JSON string to List<dynamic>
+      final List<dynamic> jsonList = json.decode(jsonString);
+      
+      // Convert to List<MovieNetworkE>
+      final List<MovieNetworkE> networkMovies = jsonList
+          .map((json) => MovieNetworkE.fromJson(json))
+          .toList();
+      
+      // Transform to List<MovieAppE>
+      return networkMovies
+          .map((networkMovie) => MovieAppE.fromNetworkEntity(networkMovie))
+          .toList();
+    } catch (e) {
+      throw Exception('Failed to load stub movies: $e');
+    }
+  }
+}
